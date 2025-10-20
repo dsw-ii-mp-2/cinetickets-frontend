7f18b523b2937bdaf76f0d83dbb26f43711fc66b
DavidReyesCuartas <david12033.dr@gmail.com>
2025-10-20 11:44:49 -0500

    Remove routing structure from main.jsx, simplifying to render only the App component

--- cinetickets/src/main.jsx
+++ cinetickets/src/main.jsx
@@ -1,19 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import Login from "./pages/Login";
-import Home from "./pages/Home";
-import Signup from "./pages/Signup";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<Signup />} />
-      <Route path="/home" element={<Home />} />
-      {/* ...other routes */}
-    </Routes>
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
