9497929e6cb34db0b30b1c468c442e762c1d79f6
DavidReyesCuartas <david12033.dr@gmail.com>
2025-10-20 11:43:31 -0500

    Refactor routing structure in main.jsx to include Login, Signup, and Home components

--- cinetickets/src/main.jsx
+++ cinetickets/src/main.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
-import "./index.css";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Signup from "./pages/Signup";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/home" element={<Home />} />
+      {/* ...other routes */}
+    </Routes>
+  </BrowserRouter>
 );
